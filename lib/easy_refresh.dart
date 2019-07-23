41d27b7f8888c1b0222840645f171dce81ea3898
luopeng <15158074877@163.com>
2019-07-23 17:45:22 +0800

    support bloc

---
+++ lib/easy_refresh.dart
@@ -0,0 +1,16 @@
+/*
+    Author: Qingyi
+    Email: xuelongqy@foxmail.com
+    createTime:2018-11-28 14:29
+ */
+library easyrefresh;
+
+export 'src/behavior/behavior.dart';
+export 'src/bloc_refresher.dart';
+export 'src/bloc_refresher_bloc.dart';
+export 'src/bloc_refresher_event.dart';
+export 'src/defines.dart';
+export 'src/footer/footer.dart';
+export 'src/header/header.dart';
+export 'src/refresh_safe_area.dart';
+export 'src/refresher.dart';
